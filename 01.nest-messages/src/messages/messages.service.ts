e88c80c660209e9fb7ae239540d91b403d2b5768
Caio Martins <dev.caiomartins@gmail.com>
2022-08-20 13:53:50 -0300

    Services and Repositories; Implementing a Repository; Reading and Writing to a Storage File; Implementing a Service

---
+++ 01.nest-messages/src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { MessagesRepository } from './messages.repository';
+
+export class MessagesService {
+  private readonly messagesRepository: MessagesRepository;
+
+  constructor() {
+    // Bad coupling
+    this.messagesRepository = new MessagesRepository();
+  }
+
+  findOne(id: string) {
+    return this.messagesRepository.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepository.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepository.create(content);
+  }
+}
