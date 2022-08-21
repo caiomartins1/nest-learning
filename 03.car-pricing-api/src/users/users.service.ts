778f93451cab4c26cc72e0bc30a402ca29b82df6
Caio Martins <dev.caiomartins@gmail.com>
2022-08-21 14:08:40 -0300

    Persisting data with typeorm; Create vs Save; Querying for data; Updating data

--- 03.car-pricing-api/src/users/users.service.ts
+++ 03.car-pricing-api/src/users/users.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const user = this.usersRepository.create({ email, password });
+    return this.usersRepository.save(user);
+  }
+
+  findById(id: number) {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+
+  findByEmail(email: string) {
+    return this.usersRepository.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findById(id);
+
+    if (!user) throw new Error('User not found');
+
+    Object.assign(user, attrs);
+
+    return this.usersRepository.save(user);
+  }
+
+  // remove() {}
+}
