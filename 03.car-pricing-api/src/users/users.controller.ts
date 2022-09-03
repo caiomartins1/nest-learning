655716ef11f57ce84e94bca2ddcff0dcea4abd9a
Caio Martins <dev.caiomartins@gmail.com>
2022-09-03 18:24:19 -0300

    current user interceptor + configuration

--- 03.car-pricing-api/src/users/users.controller.ts
+++ 03.car-pricing-api/src/users/users.controller.ts
@@ -9,16 +9,21 @@ import {
   Post,
   Query,
   Session,
+  UseInterceptors,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private readonly usersService: UsersService,
@@ -26,8 +31,8 @@ export class UsersController {
   ) {}
 
   @Get('/me')
-  me(@Session() session: any) {
-    return this.usersService.findById(session.userId);
+  me(@CurrentUser() user: User) {
+    return user;
   }
 
   @Post('/signup')
